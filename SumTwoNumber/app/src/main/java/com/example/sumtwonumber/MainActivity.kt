b980a3e034a0d8db0df62d708fc397d921715a9e
bachld2612 <lyduybach900@gmail.com>
2025-07-23 22:05:41 +0700

    finish exercise 1: Sum two number

--- SumTwoNumber/app/src/main/java/com/example/sumtwonumber/MainActivity.kt
+++ SumTwoNumber/app/src/main/java/com/example/sumtwonumber/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.sumtwonumber
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var inputA: EditText;
+    lateinit var inputB: EditText;
+    lateinit var inputKQ: EditText;
+    lateinit var btnSum: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        inputA = findViewById<EditText>(R.id.input_nhapA);
+        inputB = findViewById<EditText>(R.id.input_nhapB);
+        inputKQ = findViewById<EditText>(R.id.input_ketQua);
+        btnSum = findViewById<Button>(R.id.btn_tong);
+        btnSum.setOnClickListener {
+            val a = inputA.text.toString().toIntOrNull()?:0;
+            val b = inputB.text.toString().toIntOrNull()?:0;
+            val  c = a + b;
+            inputKQ.setText(getString(R.string.result_text, c))
         }
     }
 }
