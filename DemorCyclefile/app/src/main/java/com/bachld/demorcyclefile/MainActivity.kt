de597c756cbf771189e20306b08e0de5557a1273
bachld2612 <lyduybach900@gmail.com>
2025-07-25 11:34:43 +0700

    finish exercise 2: android application lifecycle

--- DemorCyclefile/app/src/main/java/com/bachld/demorcyclefile/MainActivity.kt
+++ DemorCyclefile/app/src/main/java/com/bachld/demorcyclefile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,36 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnDialog: Button;
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"CR424 - on destroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"CR424 - on pause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"CR424 - on restart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"CR424 - on resume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"CR424 - on start", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"CR424 - on stop", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
