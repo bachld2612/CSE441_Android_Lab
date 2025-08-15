f686796e31f609fb58b64369adc68242e9cb76d7
bachld2612 <lyduybach900@gmail.com>
2025-08-15 19:22:36 +0700

    finish exercise 22 api connection

---
+++ ApiConnection/app/src/main/java/com/bachld/apiconnection/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.bachld.apiconnection
+
+import org.xmlpull.v1.XmlPullParserException
+import android.app.Activity
+import android.os.AsyncTask
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.StringReader
+
+class MainActivity : Activity() {
+
+    private lateinit var btnParse: Button
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+    private val myList = ArrayList<String>()
+    private val URL = "https://www.w3schools.com/xml/plant_catalog.xml"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnParse = findViewById(R.id.btnparse)
+        listView = findViewById(R.id.lv1)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myList)
+        listView.adapter = adapter
+
+        btnParse.setOnClickListener {
+            LoadXMLTask().execute()
+        }
+    }
+
+    inner class LoadXMLTask : AsyncTask<Void, Void, ArrayList<String>>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            myList.clear()
+        }
+
+        override fun doInBackground(vararg params: Void?): ArrayList<String> {
+            val resultList = ArrayList<String>()
+            try {
+                val parserFactory = XmlPullParserFactory.newInstance()
+                val parser = parserFactory.newPullParser()
+
+                val xmlParser = XMLParser()
+                val xmlData = xmlParser.getXmlFromUrl(URL)
+
+                parser.setInput(StringReader(xmlData))
+
+                var eventType = parser.eventType
+                var dataShow = ""
+                var common = ""
+                var price = ""
+
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    when (eventType) {
+                        XmlPullParser.START_TAG -> {
+                            val tagName = parser.name
+                            when (tagName) {
+                                "COMMON" -> common = parser.nextText()
+                                "PRICE" -> price = parser.nextText()
+                            }
+                        }
+                        XmlPullParser.END_TAG -> {
+                            if (parser.name == "PLANT") {
+                                dataShow = "$common - $price"
+                                resultList.add(dataShow)
+                                common = ""
+                                price = ""
+                            }
+                        }
+                    }
+                    eventType = parser.next()
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return resultList
+        }
+
+        override fun onPostExecute(result: ArrayList<String>) {
+            super.onPostExecute(result)
+            myList.clear()
+            myList.addAll(result)
+            adapter.notifyDataSetChanged()
+        }
+        }
+}
